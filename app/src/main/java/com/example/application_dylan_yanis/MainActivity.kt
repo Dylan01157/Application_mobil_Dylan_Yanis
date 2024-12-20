397ce9fd6672e2d20cee78377f99354ad8634a05
Dylan01157 <dyla.boulay@gmail.com>
2024-12-20 09:12:00 +0100

    Commit_création_proet
    1er commit pour lancement du projet d'application mobil

---
+++ app/src/main/java/com/example/application_dylan_yanis/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.application_dylan_yanis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.application_dylan_yanis.ui.theme.Application_Dylan_YanisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Application_Dylan_YanisTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Application_Dylan_YanisTheme {
+        Greeting("Android")
+    }
+}
