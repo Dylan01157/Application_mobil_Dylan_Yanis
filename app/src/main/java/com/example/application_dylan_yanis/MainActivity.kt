2c980c2cf6cb63bf6507d6bfb6ab82841967d65f
Dylan01157 <dyla.boulay@gmail.com>
2025-01-04 02:44:14 +0100

    commit 1 réglage du build.gradle

--- app/src/main/java/com/example/application_dylan_yanis/MainActivity.kt
+++ app/src/main/java/com/example/application_dylan_yanis/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Application_Dylan_YanisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
